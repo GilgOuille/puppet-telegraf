30ec503ab48a71cc269c98e1098fb984e76c3461
Joris Dedieu <joris.dedieu@nfrance.com>
2018-03-23 17:32:21 +0100

    [port2freebsd-] change conf file path for freebsd

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,23 +3,24 @@
 # A set of default parameters for Telegraf's configuration.
 #
 class telegraf::params {
+  $config_dir = $::osfamily ? {
+    'windows' => 'C:/Program Files/telegraf',
+    'FreeBSD' => '/usr/local/etc/telegraf',
+    default   => '/etc/telegraf',
+  }
 
   if $::osfamily == 'windows' {
-    $config_file          = 'C:/Program Files/telegraf/telegraf.conf'
     $config_file_owner    = 'Administrator'
     $config_file_group    = 'Administrators'
-    $config_folder        = 'C:/Program Files/telegraf/telegraf.d'
-    $logfile              = 'C:/Program Files/telegraf/telegraf.log'
+    $logfile              = "$config_dir/telegraf.log"
     $manage_repo          = false
     $service_enable       = true
     $service_ensure       = running
     $service_hasstatus    = false
     $service_restart      = undef
   } else {
-    $config_file          = '/etc/telegraf/telegraf.conf'
     $config_file_owner    = 'telegraf'
     $config_file_group    = 'telegraf'
-    $config_folder        = '/etc/telegraf/telegraf.d'
     $logfile              = ''
     $manage_repo          = true
     $service_enable       = true
@@ -27,6 +28,8 @@ class telegraf::params {
     $service_hasstatus    = true
     $service_restart      = 'pkill -HUP telegraf'
   }
+  $config_file          = "$config_dir/telegraf.conf"
+  $config_folder        = "$config_dir/telegraf.d"
   $package_name           = 'telegraf'
   $ensure                 = 'present'
   $install_options        = []
