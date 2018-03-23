9dfde3c9f3fcebbe8002aea645bc1778d2ddc8ef
Joris Dedieu <joris.dedieu@nfrance.com>
2018-03-23 17:41:12 +0100

    [port2freebsd-] change conf file path for freebsd~

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,6 +7,14 @@ class telegraf::config inherits telegraf {
   assert_private()
 
   file {
+    $::telegraf::config_dir:
+      ensure  => directory,
+      owner   => $::telegraf::config_file_owner,
+      group   => $::telegraf::config_file_group,
+      mode    => '0770',
+      notify  => Class['::telegraf::service'],
+      require => Class['::telegraf::install'],
+    ;
     $::telegraf::config_file:
       ensure  => file,
       content => template('telegraf/telegraf.conf.erb'),
